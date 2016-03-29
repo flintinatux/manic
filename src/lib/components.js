d549822d96a57a4d01e8067de2aadc2d051a8337
Scott McCormack <scott.mccormack@articulate.com>
2016-03-29 01:03:27 -0400

    Prevent component name mangling when minified

--- src/lib/components.js
+++ src/lib/components.js
@@ -1,14 +1,14 @@
 const bindAll = require('lodash/bindAll');
 
-const builtIns = [
-  require('../components/motion'),
-  require('../components/render')
-];
+const builtIns = {
+  Motion: require('../components/motion'),
+  Render: require('../components/render')
+};
 
 function Components() {
   var comps = new Map;
   bindAll(comps, 'get', 'set');
-  for (var comp of builtIns) comps.set(comp.name, comp);
+  for (var name in builtIns) comps.set(name, builtIns[name]);
   return comps;
 }
 
