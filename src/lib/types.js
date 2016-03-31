553d568232d0be770dd9bd097da4a6c6fed7773c
Scott McCormack <scott.mccormack@articulate.com>
2016-03-31 09:37:29 -0400

    Lazily parse types when requested

--- src/lib/types.js
+++ src/lib/types.js
@@ -3,28 +3,29 @@ const merge = require('lodash/merge');
 const BaseEntity = require('./base-entity');
 
 function Types() {
-  var parsed = {};
+  var defs   = {},
+      parsed = {};
 
   var types = {
     get(name) {
-      return parsed[name] || BaseEntity;
+      return parseType(name);
     },
 
-    load(defs) {
+    load(newDefs) {
+      defs   = newDefs;
       parsed = {};
-      Object.keys(defs).forEach(parseType);
-
-      function parseType(name) {
-        if (!parsed[name]) {
-          var type = defs[name];
-          var parent = type.parent ? parseType(type.parent) : BaseEntity;
-          parsed[name] = merge({}, parent, type);
-          parsed[name].types.push(name);
-        }
-        return parsed[name];
-      }
     }
   };
+
+  function parseType(name) {
+    if (!parsed[name]) {
+      var type = defs[name];
+      var parent = type.parent ? parseType(type.parent) : BaseEntity;
+      parsed[name] = merge({}, parent, type);
+      parsed[name].types.push(name);
+    }
+    return parsed[name];
+  }
 
   return types;
 }
