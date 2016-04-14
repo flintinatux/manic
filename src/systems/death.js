5000efc3e527cbc9c28964ae7eeb4996b4603a8b
Scott McCormack <scott.mccormack@articulate.com>
2016-04-14 14:37:50 -0400

    Remove record of contact with dead entities

--- src/systems/death.js
+++ src/systems/death.js
@@ -3,7 +3,13 @@ module.exports = {
   phase: 'death',
   deps:  ['Death'],
 
-  update(id, [d], { entities }) {
+  update(id, [d], { comps, entities }) {
+    var contacts = comps('Contacts', id);
+
+    if (contacts) for (var other in contacts) {
+      delete comps('Contacts', other)[id];
+    }
+
     entities.remove(id);
   }
 }
